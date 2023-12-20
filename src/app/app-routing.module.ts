ef9356abf717812b35f7ab1935df1903dafb4459
ADRVDJ <adrian.cajamarca.est@tecazuay.edu.ec>
2023-12-20 00:13:02 -0500

    adrian
    ya funciona login logo y todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from 'src/login/login.component';
+import { FooterComponent } from 'src/footer/footer.component';
+
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  // ... otras rutas si es necesario
+  {path: 'footer', component: FooterComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from 'src/login/login.component';
+import { SlidebarComponent } from 'src/slidebar/slidebar.component';
+import { FooterComponent } from 'src/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
-    // Agrega otros componentes aquí si es necesario
+    LoginComponent,
+    SlidebarComponent,
+    FooterComponent
+   
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
