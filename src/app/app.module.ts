8b4d024338485b27f02bc8cb7645cdb771554b3e
¨Maikol <maikoltj11@hotmail.es>
2023-12-19 22:18:13 -0500

    Maikol
    Interfaces Rol, Periodos, Cursos, Menu Laterl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RolComponent } from './rol/rol.component';
+import { CursoComponent } from './curso/curso.component';
+import { MenuComponent } from './menu/menu.component';
+import { PeriodosComponent } from './periodos/periodos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RolComponent,
+    CursoComponent,
+    MenuComponent,
+    PeriodosComponent
   ],
   imports: [
     BrowserModule,
